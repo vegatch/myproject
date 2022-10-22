96dfafb8dafffd82e93e313794d7a69413ab8075
Stravinsky Anglade <stravinsky.anglade@mykenzie.snhu.edu>
2022-10-22 07:47:06 -0400

    adding input

--- main.js
+++ main.js
@@ -57,3 +57,58 @@ const sayHello = name =>{
 
 }
 sayHello('Bengadova')
+
+let activeSubscription = {
+    'id': 763,
+    'customerId': 328,
+    'monthlyPriceInDollars': 4  // price per active user per month
+}
+
+ let users = [
+     {
+        'id': 1,
+        'name': "Employee #1",
+        'customerId': 1,
+   
+        // when this user started
+        'activatedOn': new Date("2021-11-04"),
+    
+        // last day to bill for user
+        // should bill up to and including this date
+        // since user had some access on this date
+        'deactivatedOn': new Date("2022-04-10")
+      },
+      {
+        'id': 2,
+        'name': "Employee #2",
+        'customerId': 1,
+    
+        // when this user started
+        'activatedOn': new Date("2021-12-04"),
+    
+        // hasn't been deactivated yet
+        'deactivatedOn': null
+      },
+]
+console.log(users[0].activatedOn)
+let month = '2022-07-01'
+// let activationDate = users.map(date => date.activatedOn)
+  let monthActive = users.filter( user=> {
+        return user.activatedOn === month;
+    }).map(currentMonth=> {
+        return currentMonth.activatedOn;
+    })
+  
+console.log(monthActive) 
+
+
+function billFor(month, activeSubscription, users) {
+    let monthlyCharge =''
+  if(!activeSubscription || !users){
+    monthlyCharge = 0
+    console.log(monthlyCharge)
+  }else{
+    let activationDate = users.map(date => date.activatedOn.filter(date =>  date <= month))
+  }
+}
+billFor('2022-02')
